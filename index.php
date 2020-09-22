7b2f77a311e3a0047eee6f72f2708f5e7497f1ad
valealvarez2854 <61520291+valealvarez2854@users.noreply.github.com>
2020-09-22 12:54:05 -0500

    2taller

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="1pagina.php">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Neuton:ital@1&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <img src="img/koaj-1.png" width="30" height="30" class="d-inline-block align-top" alt="logo1" loading="lazy">
+    <h1 class="navbar-brand">koaj</h1>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">pagina principal</a>
+        </li>
+        
+          <li class="nav-item">
+            <a class="nav-link" href="1pagina.php">registro</a>
+          </li>
+          
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Busqueda" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar
+        </button>
+      </form>
+    </div>
+  </nav>
+</header>
+<mein>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/nasa.jpg" class="d-block w-100 carusel" alt="img1">
+      </div>
+      <div class="carousel-item">
+        <img src="img/2koaj.png" class="d-block w-100 carusel" alt="img2">
+      </div>
+      <div class="carousel-item">
+        <img src="img/2koaj.png" class="d-block w-100 carusel" alt="img3">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+   
+  </div>
+  <div class="container">
+  <div class="row">
+ 
+    <div class="col-12 col-md-8">
+<h1>koaj</h1>
+<p>Moda increíble a precios increíbles. Producto 100% local. Quédate en casa mientras escoges moda a precios increíbles. ¡Compra en Surtitodo online! Devoluciones gratis. Compra 100% segura. Envíos para todo el país. Compra desde casa..</p>
+<img src="img/koaj-logo.png" alt="imagen1">
+<h2>tiendas</h2>
+<p>Permoda, que opera con cien tiendas en Colombia y cuarenta distribuidas en México, Ecuador, Venezuela, Costa Rica y Perú..</p>
+<img src="img/2koaj.png" alt="img2" height="300">
+   </div>
+   <div class="col-12 col-md-4">
+  <h2>redes sociales </h2>
+  <p>instragram:koajmoda
+  facebook:koajmoda
+  pagina:koaj
+  </p>
+
+</div>
+</div>
+</div>
+
+</mein>
+<footer>
+<hr>
+<ul>
+  <li>valentina alvarez zea</li>
+  <li>instagram:vale_0813</li>
+  <li>facebook:vale alvarez</li>
+  <li>colegio parroquial carmelitano</li>
+</ul>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
